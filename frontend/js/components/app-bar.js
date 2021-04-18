d503b81df76c3e7af18b5eaed9a96da6707bc383
akasz56 <indo14nurfath@apps.ipb.ac.id>
2021-04-18 17:01:51 +0700

    adding frontend folder

---
+++ frontend/js/components/app-bar.js
@@ -0,0 +1,29 @@
+let hamburger = document.getElementById('hamburger-btn');
+hamburger.onclick = () => {
+    (hamburger.classList.contains('is-active')) ? hamburger.classList.remove('is-active'): hamburger.classList.add('is-active');
+}
+
+// hide navbar when scroll
+// let prevScrollPos = window.pageYOffset;
+// window.onscroll = () => {
+//     let appBar = document.getElementById('collNavbar'),
+//         currentScrollPos = window.pageYOffset,
+//         mobileAppBarVer = document.getElementById('hamburger-btn').getAttribute('aria-expanded');
+
+//     (prevScrollPos > currentScrollPos || mobileAppBarVer == 'true') ? appBar.style.top = "0": appBar.style.top = '-65px';
+//     prevScrollPos = currentScrollPos;
+// }
+
+// custom components
+class AppBar extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+    render() {
+        this.innerHTML = `
+            
+        `;
+    }
+}
+
+customElements.define("app-bar", AppBar);
